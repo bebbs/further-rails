b0912da26a9347be17fa5d93d0207b827aaddd44
Josh Bebbington <josh@focalwebsites.com>
2015-03-30 15:16:39 +0100

    Projects can be added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,27 @@
 class ProjectsController < ApplicationController
   def index
   end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      nil
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -1,8 +1,32 @@
 feature 'Project' do
+
+  before(:each) do
+    visit '/projects'
+  end
+
   context 'No projects have been added' do
     scenario 'Displays a prompt to add a project' do
-      visit '/projects'
       expect(page).to have_link 'Add a project'
+    end
+  end
+
+  context 'Adding a project' do
+
+    before(:each) do
+      click_link 'Add a project'
+    end
+    scenario 'Displays a form to add the project' do
+      expect(page).to have_content 'Project Name:'
+      expect(page).to have_content 'Description:'
+    end
+
+    scenario 'Accepts a project and displays it on the projects page' do
+      fill_in 'Project Name:', with: 'Widget Generator'
+      fill_in 'Description:', with: 'Sample description'
+      click_button 'Create Project'
+
+      expect(page).to have_content 'Project Name: Widget Generator'
+      expect(page).to have_content 'Sample description'
     end
   end
 end
